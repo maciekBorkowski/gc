2346917954554c4319ed8ea4dcb9bae8bbfe3c3c
Maciej Borkowski <borek199@gmail.com>
2015-03-27 10:11:23 +0100

    Added possibility to measure time of alloc and count of allocs in 60s

--- src/main/java/pl/mborkowski/gc/MyRun.java
+++ src/main/java/pl/mborkowski/gc/MyRun.java
@@ -4,11 +4,27 @@ public class MyRun implements Runnable {
 	 
     public static int n = 100;
     public boolean finished = false;
+    private String type;
+    private int index;
+    public int counter = 0;
+
+    MyRun(String type, int index) {
+        this.type = type;
+        this.index = index;
+    }
  
     public void run() {
-        for(int i = n; i >= 0; i--) {
-            MemoryEater.alloc(1);
+        if(type.equals("time")) {
+            for(int i = n; i >= 0; i--) {
+                MemoryEater.alloc(10);
+            }
+            Runner.finished.add(true);
+        } else if(type.equals("count")) {
+            while(true) {
+                MemoryEater.alloc(10);
+                Runner.counters[this.index]++;
+            }
         }
-        this.finished = true;
+        
     }
 }
--- src/main/java/pl/mborkowski/gc/Runner.java
+++ src/main/java/pl/mborkowski/gc/Runner.java
@@ -1,33 +1,51 @@
 package pl.mborkowski.gc;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Runner {
-    public static void main(String[] args) {
-    	int threadsCount = 4;
-        Runnable[] runners = new Runnable[threadsCount];
-        Thread[] threads = new Thread[threadsCount];
-        long startTime = System.nanoTime();
+    static ArrayList<Boolean> finished = new ArrayList<Boolean>();
+    static int threadsCount = 1;    
+    static int[] counters = new int[threadsCount];
+    static Runnable[] runners = new Runnable[threadsCount];
+    static Thread[] threads = new Thread[threadsCount];
+    public static void main(final String[] args) throws InterruptedException {
+    	
         
-        for(int i = 0; i < threadsCount; i++) {
-            runners[i] = new MyRun();
-            threads[i] = new Thread(runners[i]);
-            threads[i].start();
+        if(args[0].equals("time")) {
+            long startTime = System.nanoTime();
+            for(int i = 0; i < threadsCount; i++) {
+                runners[i] = new MyRun(args[0],i);
+                threads[i] = new Thread(runners[i]);
+                threads[i].start();
+            }
+            boolean finished = false;
+            while(!finished) {
+                for (Boolean finish : Runner.finished) {
+                    if(Runner.finished.size() == 1){
+                        finished = finish;
+                    } else {
+                        finished = finished & finish;    
+                    }
+                }
+            }
+            long estimatedTime = System.nanoTime() - startTime;
+            System.out.println(estimatedTime);
+        } else if(args[0].equals("count")) {
+            for(int i = 0; i < threadsCount; i++) {
+                runners[i] = new MyRun(args[0],i);
+                threads[i] = new Thread(runners[i]);
+                threads[i].start();
+            }
+            Thread.sleep(60000);
+            int countAlloc = 0;
+            for (int i = 0; i < counters.length; i++) {
+                countAlloc += counters[i];
+                threads[i].stop();
+            }
+            System.out.println(countAlloc);
         }
-        int j = 0;
-        boolean finished = false;
-		while(j <= threadsCount && !finished ) {
-//        	runners[j].run
-        	j++;
-        }
-        long estimatedTime = System.nanoTime() - startTime;
-        System.out.println(estimatedTime);
-//        runners = new Runnable[1];
-//        threads = new Thread[1];
-//        
-//        runners[0] = new MyRun(0);
-//        threads[0] = new Thread(runners[0]);
-//        startTime = System.nanoTime();
-//        threads[0].start();
-//        estimatedTime = System.nanoTime() - startTime;
-//        System.out.println(estimatedTime);
+
     }
 }
